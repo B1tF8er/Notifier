f4d6595b98173434ad72e12d094638980a830c89
Jorge Yañez <ofeth@hotmail.com>
2021-03-01 08:53:30 -0700

    Add extra configuration properties to Email

--- Notifier/EmailNotification.cs
+++ Notifier/EmailNotification.cs
@@ -9,16 +9,25 @@ namespace Notifier
 
         public string FromAddress { get; }
 
+        public string ToAddress { get; }
+
+        public string User { get; }
+
+        public string Password { get; }
+
         public EmailNotification()
         {
             Port = 8000;
             FromAddress = "some@domain.com";
+            ToAddress = "other@domain.com";
+            User = "someone";
+            Password = "5up3r53cur3";
         }
 
         public async Task Send(string message)
         {
             await Task.Delay(100).ConfigureAwait(false);
-            Console.WriteLine($"{message} via Email using port {Port} and from {FromAddress}");
+            Console.WriteLine($"{message} via Email using port {Port} and from {FromAddress} to {ToAddress} with credentials [{User}:{Password}]");
         }
     }
 }
--- Notifier/IEmailConfiguration.cs
+++ Notifier/IEmailConfiguration.cs
@@ -5,5 +5,11 @@
         int Port { get; }
 
         string FromAddress { get; }
+
+        string ToAddress { get; }
+
+        string User { get; }
+
+        string Password { get; }
     }
 }
