ddb5dfc41d05446ab0faddb3d904fce4fd1ba58e
Jorge Yañez <ofeth@hotmail.com>
2021-03-01 08:54:16 -0700

    Add GUI notification

---
+++ Notifier/GuiNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Notifier
+{
+    public class GuiNotification : INotification
+    {
+        public async Task Send(string message)
+        {
+            await Task.Delay(100).ConfigureAwait(false);
+            Console.WriteLine($"{message} via GUI");
+        }
+    }
+}
