ac07181ea1552feedb1e0396d10774f3a0082bdc
Jorge Yañez <ofeth@hotmail.com>
2021-03-15 19:48:56 -0700

    Use theories instead of facts in all other unit tests

--- Notifier.Tests/GuiNotificationShould.cs
+++ Notifier.Tests/GuiNotificationShould.cs
@@ -20,31 +20,12 @@ namespace Notifier.Tests
             sut = new Mock<GuiNotification>(messageWriter.Object);
         }
 
-        [Fact]
-        public async void Throw_ArgumentNullException_When_Message_Is_Empty()
+        [Theory]
+        [InlineData("")]
+        [InlineData(default(string))]
+        [InlineData("     ")]
+        public async void Throw_ArgumentNullException_When_Message_Is_Invalid(string message)
         {
-            var message = string.Empty;
-
-            await Assert.ThrowsAsync<ArgumentNullException>(() =>
-                sut.Object.Send(message)
-            );
-        }
-
-        [Fact]
-        public async void Throw_ArgumentNullException_When_Message_Is_Null()
-        {
-            string message = default;
-
-            await Assert.ThrowsAsync<ArgumentNullException>(() =>
-                sut.Object.Send(message)
-            );
-        }
-
-        [Fact]
-        public async void Throw_ArgumentNullException_When_Message_Is_White_Spaces()
-        {
-            var message = "     ";
-
             await Assert.ThrowsAsync<ArgumentNullException>(() =>
                 sut.Object.Send(message)
             );
