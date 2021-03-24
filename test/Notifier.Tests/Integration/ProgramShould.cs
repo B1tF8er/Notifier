d71761633407421289b8f65e8e0cbfe50003085b
Jorge Yañez <ofeth@hotmail.com>
2021-03-24 16:40:45 -0700

    Add integration test

---
+++ test/Notifier.Tests/Integration/ProgramShould.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Notifier.Tests.Integration
+{
+    public class ProgramShould
+    {
+        [Fact]
+        public async void RunSmoothly() => await Program.Main().ConfigureAwait(false);
+    }
+}
